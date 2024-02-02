8b917986378162ac29d062b531d69d03bf60db82
sanilUnstudio <sanil@unstudio.ai>
2024-02-02 23:27:38 +0530

    remove bug

--- src/pages/draw.tsx
+++ src/pages/draw.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import '../app.css';
 import { useEffect, useState, useRef, useLayoutEffect, MouseEvent } from 'react'
 import ActionBar from "../component/actionBar"
